07d777cc08eb21a6f0ddac3db3e0db5f3ef13f20
Jivan <137729176+Jivan052@users.noreply.github.com>
2024-11-26 12:13:52 +0530

    Update script.js

--- QRCodeGenerator/script.js
+++ QRCodeGenerator/script.js
@@ -3,8 +3,8 @@ function generateQRCode() {
     const link = document.getElementById('link').value;
     const qrcodeContainer = document.getElementById('qrcode');
     const emoji = document.getElementById('emoji');
-    qrcodeContainer.innerHTML = ''; // Clear previous QR code
-    emoji.innerHTML = ''; // Clear previous emoji
+    qrcodeContainer.innerHTML = ''; 
+    emoji.innerHTML = ''; 
 
     new QRCode(qrcodeContainer, link);
 }
